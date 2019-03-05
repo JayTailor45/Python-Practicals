92c2792f7657cb9da9f900d6e2baac35a8f6c18e
Jay Tailor <tailorj64@gmail.com>
2019-03-05 15:53:49 +0530

    Implemented number conversion

---
+++ 2_6.py
@@ -0,0 +1,6 @@
+value = int(input('Enter a number : '))
+
+print('Different representation of ' + str(value) + 'number')
+print('In binary : ' + bin(value))
+print('In hexadecimal : ' + hex(value))
+print('In octal : ' + oct(value))
